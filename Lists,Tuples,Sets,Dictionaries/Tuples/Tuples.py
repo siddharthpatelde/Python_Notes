f222c21e4edd71c37d018098612de8c2a98db202
Siddharth A. Patel <siddharthpatel.de@gmail.com>
2024-10-21 00:38:50 +0200

    added tuple directory and firt code

---
+++ Lists,Tuples,Sets,Dictionaries/Tuples/Tuples.py
@@ -0,0 +1,20 @@
+#list example
+List = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8']
+
+#tuple example
+Tuple = ('name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8')
+
+#or
+
+Tuple2 = 'name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8'
+
+
+print(Tuple[0])
+
+#you can not assign or change new value in Touples
+
+#Tuple[0] = 'name2'
+
+#just to make sure if the tuple is tuple
+
+print(type(Tuple2))
