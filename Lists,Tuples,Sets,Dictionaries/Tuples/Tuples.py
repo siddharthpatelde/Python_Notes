5bcb69101daa301da5635c8adbece2f70ac3c18c
Siddharth A. Patel <siddharthpatel.de@gmail.com>
2024-10-21 00:47:01 +0200

    tuple added inbuilt function picture

--- Lists,Tuples,Sets,Dictionaries/Tuples/Tuples.py
+++ Lists,Tuples,Sets,Dictionaries/Tuples/Tuples.py
@@ -18,3 +18,19 @@ print(Tuple[0])
 #just to make sure if the tuple is tuple
 
 print(type(Tuple2))
+
+#we just saw that we can not add any items to tuples but we can add two tuples together
+
+tuple_1 = 1,2,3
+tuple_2 = 'city','street','house'
+
+tuple_3 = tuple_1 + tuple_2
+
+print(tuple_3)
+
+#we can not remove items in tuple, but we can permanently delete the tuple
+
+del tuple_3
+#print(tuple_3)
+
+
