e90c1e80d555cd1b18b5ef634e1ab421fcacdef6
Siddharth Patel <141241898+siddharthpatelde@users.noreply.github.com>
2024-10-22 15:14:13 +0200

    more stuff added on dictionary.py

--- Lists,Tuples,Sets,Dictionaries/Dictionaries/Dictionaries.py
+++ Lists,Tuples,Sets,Dictionaries/Dictionaries/Dictionaries.py
@@ -11,4 +11,35 @@
 #now lets create the python dictionary
 order = {"item_type": "phone", "manufacturer": "apple",
          "seller": "tech-world", "year": 2024, "item_id": (20.453, 12.123)}
+print(type(order)) #verify that our order is of type dictionary
 
+# Second way to defining the Dictionary in python is using dict() function
+order2 = dict(type = "laptop", manufacturer = "apple", year = 2024, ram = 32)
+print(order2)
+
+#add additional elements to our dictionary using brackets
+order2["order_number"] = 234124
+order2["order_id"] = "45645342"
+print(order2)
+
+#accsessing the data from dictionary
+print(order2["order_number"])
+#print(order2["order"]) in this case we will get error
+
+#to prevent key error we can use try or n operator
+
+if 'age' in order2:
+    print(order2['age'])
+else:
+    print("the key value not found in dictionary")
+
+#or using try
+
+try:
+    print(order2['age'])
+except KeyError:                                        #type of error
+    print("the key value not found in dictionary")
+
+#lets print all the data in dictorary
+for key in order2:
+    print(f"{key}: {order2[key]}")
