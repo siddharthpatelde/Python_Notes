b8d52a5de5ef7eeaf68e51a14af8ad1995780bd8
Siddharth Patel <141241898+siddharthpatelde@users.noreply.github.com>
2024-10-22 15:24:06 +0200

    added one more method to check the keys in dictionary

--- Lists,Tuples,Sets,Dictionaries/Dictionaries/Dictionaries.py
+++ Lists,Tuples,Sets,Dictionaries/Dictionaries/Dictionaries.py
@@ -43,3 +43,21 @@ except KeyError:                                        #type of error
 #lets print all the data in dictorary
 for key in order2:
     print(f"{key}: {order2[key]}")
+
+#in order to see what you can do with dictionary do this first
+print(dir(order2))
+
+#output may look like this:['__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__',
+#                           '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__',
+#                            '__hash__', '__init__', '__init_subclass__', '__ior__', '__iter__', '__le__', '__len__',
+#                            '__lt__', '__ne__', '__new__', '__or__', '__reduce__', '__reduce_ex__', '__repr__',
+#                            '__reversed__', '__ror__', '__setattr__', '__setitem__', '__sizeof__', '__str__',
+#                            '__subclasshook__', 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop',
+#                            'popitem', 'setdefault', 'update', 'values']
+
+#to see how any method works use help() function
+
+#help(order2.get)
+
+check_age = order2.get("age", "age was not specified")
+print(check_age)
