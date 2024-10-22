902400e0c4089da77a0df96eca3b1984117752c3
Siddharth Patel <141241898+siddharthpatelde@users.noreply.github.com>
2024-10-22 14:52:07 +0200

    create dictionary directory and file

---
+++ Lists,Tuples,Sets,Dictionaries/Dictionaries/Dictionaries.py
@@ -0,0 +1,14 @@
+#let say we are making website like amazon and it has sevral key datas  like following
+
+#Online order
+
+#item_type = "phone"
+#manufacturer = "apple"
+#seller = "tech-world"
+#year = 2024
+#item_id = (20.453, 12.123)
+
+#now lets create the python dictionary
+order = {"item_type": "phone", "manufacturer": "apple",
+         "seller": "tech-world", "year": 2024, "item_id": (20.453, 12.123)}
+
